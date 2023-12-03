be11839b7e4acbe9f9ffd52a1db8ee5a01b6d98c
pajowu <git@ca.pajowu.de>
2023-12-04 00:07:52 +0100

    Bump version

--- common.nix
+++ common.nix
@@ -4,12 +4,12 @@
   name = "transcribee";
   version = "0.0.1";
   source = fetchzip {
-    url = "https://github.com/bugbakery/transcribee/archive/c81003d82b42704fdcb5705ab2cd02683dc2f161.tar.gz";
-    hash = "sha256-gUF3CdtumBgpvLVgxbMkBORflNM8gfuNEBv3vqgusyU=";
+    url = "https://github.com/bugbakery/transcribee/archive/25a8fb2aaf6f3e587a8af58160b2a1d85f124768.tar.gz";
+    hash = "sha256-j9A5zl0YabI6klh+sCBOySS/gYeZ0s8QF8EdMEgvk+I=";
   };
   frontendSrc = fetchzip {
-    url = "https://github.com/bugbakery/transcribee-frontend-releases/raw/main/frontend-c81003d82b42704fdcb5705ab2cd02683dc2f161.tar.gz";
-    hash = "sha256-om3ehHHKedEchenNE6ACDUHvhGTw65U8U144VphIArk=";
+    url = "https://github.com/bugbakery/transcribee-frontend-releases/raw/main/frontend-25a8fb2aaf6f3e587a8af58160b2a1d85f124768.tar.gz";
+    hash = "sha256-G5bR/T9azyK0RspbxLHiHITWvn3tvSAy0u3eXMeh4Es=";
     stripRoot = false;
   };
 }
