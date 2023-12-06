bd4e5006a70571f711f4ea515c4b55a2193dc015
Philipp Mandler <info@philipp-mandler.com>
2023-12-06 17:46:54 +0100

    Bump version

--- common.nix
+++ common.nix
@@ -4,12 +4,12 @@
   name = "transcribee";
   version = "0.0.1";
   source = fetchzip {
-    url = "https://github.com/bugbakery/transcribee/archive/5e5646543d2e4297327980acef6b66544ec6ddc5.tar.gz";
-    hash = "sha256-d+revIc8ovqYlhbEQdpu06xx2xj1hCSj0XGW18PFjWE=";
+    url = "https://github.com/bugbakery/transcribee/archive/1909fcdf6d4d495f2f6b67b90bfbcdcdc1a1a5fb.tar.gz";
+    hash = "sha256-5TTQp7/zMuyWqx/4Mfz9v/CtCXy/Li6ZkLDcUpNdpzs=";
   };
   frontendSrc = fetchzip {
-    url = "https://github.com/bugbakery/transcribee-frontend-releases/raw/main/frontend-5e5646543d2e4297327980acef6b66544ec6ddc5.tar.gz";
-    hash = "sha256-7L3U5ef43MmvjynOhVEB7BfF1nKNHeGsEfg/LtADCRA=";
+    url = "https://github.com/bugbakery/transcribee-frontend-releases/raw/main/frontend-1909fcdf6d4d495f2f6b67b90bfbcdcdc1a1a5fb.tar.gz";
+    hash = "sha256-b6N6GFFzaCsvS94Uf9+A7JdtM89bpEeddfbsRIoE6s8=";
     stripRoot = false;
   };
 }
